7e127c8d8bccfb04377eca3ac33f2d25337a0f26
dumitru <dumitrudeleste@gmail.com>
2021-10-23 20:25:23 +0200

    Ejercicio 320 completo, la estructura basica creada,

---
+++ Soporte.php
@@ -0,0 +1,46 @@
+<?php
+
+class Soporte {
+
+    //-----------------------------Atributos----------------------------
+    public $titulo;
+    protected $numero;
+    private $precio;
+
+    private const IVA = 21;
+
+    //NOTA: no encuentro en ningun sitio de como hacer una constante privada y estatica
+    // en java se puede hacer asi: Public static final nombre_constante;
+    //---------------------------Constructor-------------------------------
+    public function __construct($titulo, $numero, $precio) {
+        $this->titulo = $titulo;
+        $this->numero = $numero;
+        $this->precio = $precio;
+    }
+
+    //----------------------------metodos-------------------------------
+    public function getPrecio() {
+        return $this->precio;
+    }
+
+    public function getPrecioConIVA() {
+        $precio_iva = $this->precio + ($this->precio * 0.21);
+        return $precio_iva;
+    }
+
+    public function getNumero() {
+        return $this->numero;
+    }
+
+    public function muestraResumen() {
+        echo '<br> ------Resumen----';
+        echo "<br>Titulo: " . $this->titulo;
+        echo "<br>Precio sin IVA = " . $this->getPrecio() . " euros";
+        echo "<br>Precio con IVA = " . $this->getPrecioConIVA() . " euros";
+        echo "<br>Numero = " . $this->getNumero();
+        echo '<br> --------------------';
+    }
+
+}
+?>
+
