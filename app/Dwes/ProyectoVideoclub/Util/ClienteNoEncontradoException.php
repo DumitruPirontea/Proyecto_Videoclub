abba0be4ce249fb812c6acff7f6bea247914b9ab
dumitru <dumitrudeleste@gmail.com>
2021-11-09 09:33:05 +0100

    333, creadas clases de excepciones

---
+++ app/Dwes/ProyectoVideoclub/Util/ClienteNoEncontradoException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Dwes\ProyectoVideoclub\Util;
+
+class ClienteNoEncontradoException extends VideoclubException{
+    
+}
+
+?>
+
---
+++ app/Dwes/ProyectoVideoclub/Util/SoporteNoEncontradoException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Dwes\ProyectoVideoclub\Util;
+
+class SoporteNoEncontradoException extends VideoclubException{
+    
+}
+
+?>
+
