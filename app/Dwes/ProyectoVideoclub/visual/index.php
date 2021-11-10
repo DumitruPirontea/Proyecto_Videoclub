8e5f404b6ef294e90adbdde81474a67ace93bce0
dumitru <dumitrudeleste@gmail.com>
2021-11-10 18:53:57 +0100

    420, creados todos los archivos y funcional todo

---
+++ app/Dwes/ProyectoVideoclub/visual/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link href="estilos.css" rel="stylesheet" type="text/css">
+        <title>Document</title>
+    </head>
+    <body>
+
+        <form action='login.php' method='post'>
+            <fieldset>
+                <legend>Login</legend>
+                <div><span class='error'><?php if(isset($error)){echo $error;} ?></span></div>
+                <div>
+                    <label for='usuario'>Usuario:</label><br />
+                    <input type='text' name='inputUsuario' id='usuario' maxlength="50" /><br />
+                </div>
+                <div>
+                    <label for='password'>Contraseña:</label><br />
+                    <input type='password' name='inputPassword' id='password' maxlength="50" /><br />
+                </div>
+                <div>
+                    <input type='submit' name='enviar' value='Enviar' />
+                </div>
+            </fieldset>
+        </form>
+
+    </body>
+</html>
